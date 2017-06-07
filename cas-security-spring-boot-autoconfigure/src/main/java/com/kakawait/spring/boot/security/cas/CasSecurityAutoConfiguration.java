11614fccf423a3e109aabcfd3f9b5bd0196b6224
Thibaud Leprêtre <thibaud.lepretre@gmail.com>
2017-06-07 11:28:32 +0200

    Add @Order to do not override actuator security
    Without `@Order` our `CasLoginSecurityConfiguration` overrides actuator security, thus no-auth endpoints like `/health` was authenticated.
    
    Simply add `@Order` to fix it.
    
    fixes #1

--- cas-security-spring-boot-autoconfigure/src/main/java/com/kakawait/spring/boot/security/cas/CasSecurityAutoConfiguration.java
+++ cas-security-spring-boot-autoconfigure/src/main/java/com/kakawait/spring/boot/security/cas/CasSecurityAutoConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.cas.ServiceProperties;
 import org.springframework.security.cas.authentication.CasAuthenticationProvider;
@@ -191,6 +192,7 @@ public class CasSecurityAutoConfiguration {
         }
     }
 
+    @Order(SecurityProperties.BASIC_AUTH_ORDER - 1)
     static class CasLoginSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         private final CasAuthenticationFilterConfigurer filterConfigurer = new CasAuthenticationFilterConfigurer();
